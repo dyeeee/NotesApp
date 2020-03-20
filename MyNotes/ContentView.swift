e13b516e6f330cfaee62af72059b0f5fb7b7fde4
dyeeee <duye99@outlook.com>
2020-03-20 10:26:25 +0800

    Initial Commit

---
+++ MyNotes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNotes
+//
+//  Created by YES on 2020/3/20.
+//  Copyright © 2020 YES. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
